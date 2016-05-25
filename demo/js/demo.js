d8d37083baa294c3e752a3fb53a05423379ad1d9
Andrew_Okorokov <andrewokorokov@gmail.com>
2016-05-25 16:41:17 +0300

    demo

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -20,11 +20,15 @@ var demo = [{
 	theme: 'Stats report: Profit & Lose',
 	speaker: people.columbus,
 	desc: ''
-}, {
-	theme: 'Social sharing cards',
-	speaker: people.goodluck,
-	desc: ''
-}, {
+},
+
+// {
+// 	theme: 'Social sharing cards',
+// 	speaker: people.goodluck,
+// 	desc: ''
+// },
+
+{
 	theme: 'Intercom: New fields',
 	speaker: people.goodluck,
 	desc: ''
