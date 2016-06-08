80ea00d8d8cb223cf5b801a4507eb3a733206f76
Andrew_Okorokov <andrewokorokov@gmail.com>
2016-06-08 12:12:01 +0300

    demo

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,39 +1,39 @@
 'use strict';
 
 var demo = [{
-	theme: 'Twitter cards',
+	theme: 'Twitter cards for MC sites',
 	speaker: people.goodluck,
 	desc: ''
 }, {
-	theme: 'Live show monitoring',
+	theme: 'Live show monitoring interface',
 	speaker: people.goofy,
 	desc: ''
 }, {
-	theme: 'Filter ZCP & Stats',
+	theme: 'Zipped packages model filter + stats',
 	speaker: people.nightwoolf,
 	desc: ''
 }, {
-	theme: 'MCProfits',
+	theme: 'MCProfits promo site & admin panel design',
 	speaker: people.subzero,
 	desc: ''
 }, {
-	theme: 'MCProfits opt-in',
-	speaker: [people.goodluck, people.nightwoolf],
+	theme: 'Models opt-in to MCProfits',
+	speaker: [people.goodluck, people.nightwoolf, people.knoxville],
 	desc: ''
 }, {
-	theme: 'Free hosted galleries',
+	theme: 'Free hosted galleries ',
 	speaker: [people.nightwoolf, people.knoxville, people.kai],
 	desc: ''
 }, {
-	theme: 'Traffic tags',
+	theme: 'Traffic tags stats for affiliates',
 	speaker: people.columbus,
 	desc: ''
 }, {
-	theme: 'Legal docs',
+	theme: 'MC legal docs update',
 	speaker: people.subzero,
 	desc: ''
 }, {
-	theme: 'Load speed optimization',
+	theme: 'Load speed optimisation for model websites',
 	speaker: people.alexp,
 	desc: ''
 }];
