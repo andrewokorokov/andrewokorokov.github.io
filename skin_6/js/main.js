b10c0ec81a9f2fd460def6ba508d1c29459d2526
Andrew_Okorokov <andrewokorokov@gmail.com>
2016-07-11 11:11:10 +0300

    updates

--- skin_6/js/main.js
+++ skin_6/js/main.js
@@ -1,30 +1,58 @@
 'use strict';
 
+/*
+ *		Slider actions
+ */
+
 var slider = {
 	images: ['url( ../frame_01.jpg )', 'url( ../frame_03.jpg )', 'url( ../frame_04.jpg )'],
-
-	counter: 0,
 
 	interval: 7000,
 
 	animate: function animate() {
 		setInterval(function () {
 			getId('slide_' + this.counter).style.opacity = '0';
+			getClass('trigger-points-item', this.counter).style.opacity = '1';
 
-			if (this.counter > this.images.length - 1) {
-				getId('slide_' + this.counter).style.opacity = '0';
-				this.counter = 0;
-			} else {
+			if (this.counter < this.images.length - 1) {
 				this.counter++;
+			} else {
+				this.counter = 0;
 			}
 
 			getId('slide_' + this.counter).style.opacity = '1';
+			getClass('trigger-points-item', this.counter).style.opacity = '.3';
 		}.bind(this), this.interval);
 	},
 
 	init: function init() {
-		getId('slide_' + this.counter).style.opacity = '1';
+		this['counter'] = 0;
 
+		var slides = document.createElement('ul');
+		var points = document.createElement('ul');
+		slides.classList.add('slides');
+		points.classList.add('trigger-points');
+
+		for (var x = 0; x < this.images.length; x++) {
+			createNode({
+				node: 'li',
+				classes: 'slides-item',
+				id: 'slide_' + x,
+				parent: slides
+			});
+
+			createNode({
+				node: 'li',
+				classes: 'trigger-points-item',
+				parent: points
+			});
+		}
+
+		getClass('points').insertAdjacentElement('beforeEnd', points);
+		getClass('slider').insertAdjacentElement('afterBegin', slides);
+
+		getId('slide_' + this.counter).style.opacity = '1';
+		getClass('trigger-points-item', this.counter).style.opacity = '.3';
 		setTimeout(this.animate(), this.interval);
 	}
 };
@@ -32,44 +60,45 @@ var slider = {
 window.onload = function () {
 	slider.init();
 };
+
 /*
  *	Sidebar actions
  */
 
 var trigger = false;
 
-listener(getId('menu-icon'), 'click', function () {
+catchClick(getId('menu-icon'), function () {
 	getId('sidebar-menu').style.left = '0';
 	getId('shop-menu').style.left = '0';
 	event.stopPropagation();
 	trigger = true;
 });
 
-listener(getClass('main-menu-item', 8), 'mouseover', function () {
+catchMouseOver(getClass('main-menu-item', 8), function () {
 	getId('shop-menu').style.left = '300px';
 });
 
-listener(getId('sidebar-menu'), 'mouseover', function () {
+catchMouseOver(getId('sidebar-menu'), function () {
 	getTag('body').style.cursor = 'default';
 });
 
-listener(getId('sidebar-menu'), 'mouseout', function () {
+catchMouseOut(getId('sidebar-menu'), function () {
 	getTag('body').style.cursor = 'url(./img/close_cursor.png), auto';
 });
 
-listener(getId('shop-menu'), 'mouseover', function () {
+catchMouseOver(getId('shop-menu'), function () {
 	getTag('body').style.cursor = 'default';
 });
 
-listener(getId('shop-menu'), 'mouseout', function () {
+catchMouseOut(getId('shop-menu'), function () {
 	getTag('body').style.cursor = 'url(./img/close_cursor.png), auto';
 });
 
-listener(getId('sidebar-menu'), 'click', function () {
+catchClick(getId('sidebar-menu'), function () {
 	event.stopPropagation();
 });
 
-listener(document, 'click', function () {
+catchClick(document, function () {
 	if (trigger === true) {
 		getId('sidebar-menu').style.left = '-300px';
 		getId('shop-menu').style.left = '-300px';
@@ -104,6 +133,41 @@ function getTag(element, index) {
 	return document.getElementsByTagName(element)[index];
 }
 
-function listener(element, action, callback) {
-	element.addEventListener(action, callback);
+function catchClick(element, callback) {
+	element.addEventListener('click', callback);
+}
+
+function catchMouseOver(element, callback) {
+	element.addEventListener('mouseover', callback);
+}
+
+function catchMouseOut(element, callback) {
+	element.addEventListener('mouseout', callback);
+}
+
+function createNode(element) {
+	element.node = element.node;
+	element.id = element.id || undefined;
+	element.classes = element.classes || undefined;
+	element.parent = element.parent || undefined;
+
+	var node = document.createElement(element.node);
+
+	if (element.id) {
+		node.setAttribute('id', element.id);
+	}
+
+	if (element.classes) {
+		if (Array.isArray(element.classes)) {
+			element.classes.forEach(function (name) {
+				node.classList.add(name);
+			});
+		} else {
+			node.classList.add(element.classes);
+		}
+	}
+
+	if (element.parent) {
+		element.parent.appendChild(node);
+	}
 }
